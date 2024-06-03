be4c4023c6c4701aa3c146efce2660f281e74111
RobsonCoura <robson_coura@yahoo.com.br>
2024-06-03 13:41:24 -0300

    Implementacao de linhas de comentario no codigo

--- PycharmProjects/myPythonProject/cap_07/MeuPrimeiroSocket.py
+++ PycharmProjects/myPythonProject/cap_07/MeuPrimeiroSocket.py
@@ -1,8 +1,9 @@
-import socket
+import socket  # Importa o módulo de rede 'socket'.
 
-resp="S"
-while(resp=="S"):
-    url=input("Digite uma url: ")
-    ip=socket.gethostbyname(url)
-    print("O IP referente a url informada é: ", ip)
-    resp=input("Digite <S> para continuar: ").upper()
+resp = "S"  # Define a variável de controle 'resp' com valor inicial "S".
+
+while (resp == "S"):  # Continua executando enquanto 'resp' for "S".
+    url = input("Digite uma url: ") # Solicita ao usuário uma URL.
+    ip = socket.gethostbyname(url) # Obtém o endereço IP da URL.
+    print("O IP referente a url informada é: ", ip) # Exibe o endereço IP.
+    resp = input("Digite <S> para continuar: ").upper() # Pergunta se o usuário deseja continuar e converte a resposta para maiúsculas.
